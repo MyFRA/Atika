214523fd2d8a326245ad4dff4d953e3c0d21ac53
MyFRA <tomyntapss@gmail.com>
2023-10-11 14:47:44 +0700

    feat: base sidebar

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,14 @@
 import { defineConfig } from 'astro/config';
 
+import tailwind from "@astrojs/tailwind";
+
 // https://astro.build/config
 export default defineConfig({
     compressHTML: false,
     build: {
         format: 'file'
-    }
+    },
+    integrations: [tailwind({
+        applyBaseStyles: false,
+    })]
 });
---
+++ dist/js/atika.js
@@ -0,0 +1,104 @@
+
+document.addEventListener('DOMContentLoaded', () => {
+    renderFunctionalitySidebarItemDropdown()
+    reloadChevronActiveAfterReloaded()
+})
+
+/**
+ * Sidebar Submenu Accordion
+ * 
+ */
+const renderFunctionalitySidebarItemDropdown = () => {
+    const submenuItemElements = document.querySelectorAll('.submenu-item')
+    submenuItemElements.forEach((submenuItemElement) => {
+        submenuItemElement.addEventListener('click', () => {
+            const submenuItemAccordElement = submenuItemElement.querySelector('ul')
+            const submenuItemChevronElement = submenuItemElement.querySelector('.submenu-item-chevron')
+            submenuItemElement.classList.toggle('active')
+
+            if (submenuItemAccordElement) {
+
+                if (submenuItemElement.classList.contains('active')) {
+                    !submenuItemAccordElement.classList.contains('active') ? submenuItemAccordElement.classList.add('active') : ''
+                } else {
+                    submenuItemAccordElement.classList.contains('active') ? submenuItemAccordElement.classList.remove('active') : ''
+                }
+
+                if (submenuItemAccordElement.classList.contains('active')) {
+                    submenuItemChevronElement.innerHTML =
+                        `
+                        <span class="bg-gray-300 p-1 rounded-full">
+                            <svg 
+                                xmlns="http://www.w3.org/2000/svg" 
+                                fill="none" 
+                                viewBox="0 0 24 24" 
+                                stroke-width="1.5" 
+                                stroke="currentColor" 
+                                class="w-4 h-4"
+                            >
+                                <path 
+                                    stroke-linecap="round" 
+                                    stroke-linejoin="round" 
+                                    d="M19.5 8.25l-7.5 7.5-7.5-7.5" 
+                                />
+                            </svg>
+                        </span>
+                    `
+                } else {
+                    submenuItemChevronElement.innerHTML =
+                        `
+                        <span class="bg-gray-100 p-1 rounded-full">
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                fill="none"
+                                viewBox="0 0 24 24"
+                                stroke-width="1.5"
+                                stroke="currentColor"
+                                class="w-4 h-4"
+                            >
+                                <path
+                                    stroke-linecap="round"
+                                    stroke-linejoin="round"
+                                    d="M8.25 4.5l7.5 7.5-7.5 7.5"
+                                ></path>
+                            </svg>
+                        </span>
+                    `
+                }
+            }
+        })
+    })
+}
+
+/**
+ * Reload Chevron Active After Reloaded
+ * 
+ */
+const reloadChevronActiveAfterReloaded = () => {
+    const submenuItemChevronElements = document.querySelectorAll('.submenu-item-chevron')
+
+    submenuItemChevronElements.forEach((submenuItemChevronElement) => {
+        if (submenuItemChevronElement.parentElement.parentElement.classList.contains('active')) {
+            submenuItemChevronElement.innerHTML =
+                `
+            <span class="bg-gray-300 p-1 rounded-full">
+                <svg 
+                    xmlns="http://www.w3.org/2000/svg" 
+                    fill="none" 
+                    viewBox="0 0 24 24" 
+                    stroke-width="1.5" 
+                    stroke="currentColor" 
+                    class="w-4 h-4"
+                >
+                    <path 
+                        stroke-linecap="round" 
+                        stroke-linejoin="round" 
+                        d="M19.5 8.25l-7.5 7.5-7.5-7.5" 
+                    />
+                </svg>
+            </span>
+        `
+        }
+    })
+}
+
