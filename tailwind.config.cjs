4605d1dab8c6c8f260ece6f94d7c1862be1c8280
MyFRA <tomyntapss@gmail.com>
2023-10-18 19:40:57 +0700

    adding preline UI

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,11 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+    content: [
+        './src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}',
+        'node_modules/preline/dist/*.js'
+    ],
     theme: {
         fontFamily: {
             sans: "'Inter', sans-serif"
         },
         extend: {},
     },
-    plugins: [require("daisyui")],
+    plugins: [require("daisyui"), require('preline/plugin'),],
 }
